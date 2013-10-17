317e7c271f1831e3fb653647613707bd69a6a937
benzhemin <benzhemin@gmail.com>
2013-10-17 16:01:31 +0800

    add ActionSheetPicker and TCUtils with slightly revise

---
+++ Acquirer/api/system/utils/NSDate+TCUtils.h
@@ -0,0 +1,12 @@
+//
+//  NSDate+TCUtils.h
+//
+//  Created by Tim Cinel on 2011-11-14.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate(TCUtils)
+- (NSDate *)TC_dateByAddingCalendarUnits:(NSCalendarUnit)calendarUnit amount:(NSInteger)amount;
+@end
